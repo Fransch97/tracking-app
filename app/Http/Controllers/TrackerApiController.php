e0a705e53cfc2d28b47d799509f8980f91fbcf85
fransch97 <fransch345@gmail.com>
2022-08-11 16:55:57 +0200

    small changes

--- app/Http/Controllers/TrackerApiController.php
+++ app/Http/Controllers/TrackerApiController.php
@@ -13,13 +13,17 @@ class TrackerApiController extends Controller
     public function trackme(Request $req){
         $new_track = new TrackerApi();
         $new_track->ip_adress = $req->ip();
+
+        //get location
         $new_track->locations = Location::get($new_track->ip_adress);
+
+        //get device info
         $os_data = ['Windows','Mac','Linux','GNU'];
         foreach($os_data as $os){
             if (str_contains($_SERVER['HTTP_USER_AGENT'], $os)){
                 $new_track->OS = $os;
             }else{
-
+                $new_track->OS = "Not defined";
             }
         }
         $agent = new Agent;
@@ -43,17 +47,22 @@ class TrackerApiController extends Controller
           $result = 'Mobile';
         }
         $new_track->device = $result;
+
+        //get referer
         if(empty($_SERVER['HTTP_REFERER'])){
 
             $new_track->refferer = "127.0.0.1"         ;
         }else{
             $new_track->refferer = $_SERVER['HTTP_REFERER'];
         }
+
         $new_track->url = $req->url();
         $new_track->language = $req->getLocale();
         $new_track->save();
-        return response()->json($new_track);
-        // return redirect()->away('https://www.google.com');
+
+
+        // return response()->json($new_track);
+        return redirect()->away('https://www.additive.eu/en/');
     }
 }
 
--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//tracking link
 Route::get('/trcklnk-additive', 'TrackerApiController@trackme');
